2924f874b5dc109c8e88fd98021f2e4688a56e6f
Heather Caputo <caputoheather@ymail.com>
2018-11-12 17:29:14 +0000

    Initial Commit

---
+++ byotest.py
@@ -0,0 +1,20 @@
+
+def test_are_equal(actual, expected):
+    assert expected == actual, "Expected {0}, got {1}".format(expected, actual)
+    
+def test_not_equal(a, b):
+    assert a != b, "Did not expect {0} but got {1}".format(a, b)
+    
+def test_is_in(collection, item):
+    assert item in collection, "{0} does not contain {1}".format(collection, item)
+    
+def test_not_in(collection, item):
+    assert item not in collection, "{0} does not contain {1}".format(collection, item)
+    
+def test_between(lower_limit, upper_limit, actual):
+    """
+    Check to ensure that a number is between two other numbers. Raises
+    AssertionError if the number is not between the other two numbers
+    """
+    assert upper_limit <= actual <= upper_limit, "{0} is not between {1} and {2}".format(actual, upper_limit, lower_limit)
+
